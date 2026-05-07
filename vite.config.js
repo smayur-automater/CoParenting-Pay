f4273b1e05f886cb83c3af3faf5a133e6cbe4b10
smayur-automater <smayur0183@gmail.com>
2026-05-08 08:14:46 +1000

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve, dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   plugins: [react()],
-  build: { outDir: 'dist' }
+  root: __dirname,
+  css: {
+    postcss: resolve(__dirname, 'postcss.config.js'),
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
 })
